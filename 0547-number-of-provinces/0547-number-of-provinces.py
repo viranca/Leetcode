11f2959005e7bedf9877101563b62e36abee2893
viranca <37591728+viranca@users.noreply.github.com>
2025-03-09 18:31:47 -0400

    Time: 3 ms (90%), Space: 19 MB (69.98%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,44 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [0] * size
+        
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+
+        return self.parent[x]
+
+
+    def union_set(self, x, y):
+        xset = self.find(x)
+        yset = self.find(y)
+
+        if self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = yset
+
+        elif self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+
+        else:
+            self.parent[yset] = xset
+            self.rank[xset] += 1
+    
+    
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        uf = UnionFind(n)
+        numberOfComponents = n
+
+        for i in range(n):
+            for j in range(i+1, n):
+                if isConnected[i][j] == 1 and uf.find(i) != uf.find(j):
+                    numberOfComponents -= 1
+                    uf.union_set(i, j)
+        
+        return numberOfComponents
+
+        
+        
+        
